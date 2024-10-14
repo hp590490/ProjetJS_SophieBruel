ce3764c703e0062c4098dae8e8fd00144ddf2d81
Hugo PLASTRE <hugo.plastre@gmail.com>
2024-10-14 17:53:37 +0200

    step 2's done

---
+++ loginPage/script.js
@@ -0,0 +1,146 @@
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll(
+  "input[type=text], input[type=password]"
+);
+let email, password;
+
+// on va créer une fonction d'affichage en cas d'erreur dans les inputs
+function errorDisplay(tag, message, valid) {
+  const container = document.querySelector("." + tag + "container");
+  const span = document.querySelector("." + tag + "container > span");
+
+  if (!valid) {
+    container.classList.add("error");
+    span.textContent = message;
+  } else {
+    container.classList.remove("error");
+    span.textContent = message;
+  }
+}
+// on va créer une fonction pour vérifier la valeur dans l'input email
+function emailChecker(value) {
+  if (!value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i)) {
+    errorDisplay("email", "Le mail n'est pas valide");
+    email = null;
+  } else {
+    errorDisplay("email", "", true);
+    email = value;
+  }
+}
+
+// on va créer une fonction pour vérifier la valeur dans l'input password
+
+function passwordChecker(value) {
+  if (!value.match(/^(?=.*?[A-Z])(?=.*\d)(?=.*?[a-z]).{6,}$/)) {
+    errorDisplay(
+      "password",
+      "Minimum de 6 caractères et une lettre majuscule (caractères spéciaux interdits)"
+    );
+    password = null;
+  } else {
+    errorDisplay("password", "", true);
+    password = value;
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "email":
+        emailChecker(e.target.value);
+        break;
+      case "password":
+        passwordChecker(e.target.value);
+        break;
+      default:
+        null;
+    }
+  });
+});
+
+form.addEventListener("submit", (e) => {
+  if (email && password) {
+    e.preventDefault();
+    let userData = {
+      email: `${email}`,
+      password: `${password}`,
+    };
+    async function loginUser(userData) {
+      const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+
+      const responseData = await response.json();
+      // responseData contient maintenant les données
+      if (responseData.message === "user not found") {
+        e.preventDefault();
+        const idError = document.querySelector(".iderror");
+        idError.innerHTML = `<p>Utilisateur inconnu, veuillez vérifier votre e-mail et/ou votre mot de passe.</p>`;
+        idError.style.color = "red";
+        idError.style.marginBottom = "20px";
+      } else if (responseData.error) {
+        e.preventDefault();
+        const idError = document.querySelector(".iderror");
+        idError.innerHTML = `<p>Utilisateur inconnu, veuillez vérifier votre e-mail et/ou votre mot de passe.</p>`;
+        idError.style.color = "red";
+        idError.style.marginBottom = "20px";
+      } else {
+        sessionStorage.setItem("authToken", responseData.token);
+        window.location.href = "../FrontEnd/index.html";
+      }
+      return responseData;
+    }
+    // Appel de la fonction
+    (async () => {
+      const data = await loginUser(userData);
+      console.log(data); // Utiliser les données en dehors de la fonction si besoin
+    })();
+  } else {
+    e.preventDefault();
+    alert("Veuillez remplir correctement les champs");
+  }
+});
+//on garde dans le local storage
+// // localStorage.setItem("e-mail", email);
+// localStorage.setItem("password", password);
+// localStorage.removeItem("user");
+// console.log(email, password);
+
+// const formData = new FormData(form);
+// const data = Object.fromEntries(formData);
+
+// form.addEventListener("submit", (e) => {
+//   if (email && password) {
+//     e.preventDefault();
+//     let userData = {
+//       email: `${email}`,
+//       password: `${password}`,
+//     };
+//     fetch("http://localhost:5678/api/users/login", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(userData),
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         responseData = data;
+//         console.log(responseData);
+//         if (responseData === false) {
+//           alert("User inconnu, veuillez vérifier.");
+//         }
+//       });
+
+//     //on remet les inputs vides après validation du form
+//     email = null;
+//     password = null;
+//     inputs.forEach((input) => (input.value = ""));
+//   } else {
+//     alert("Veuillez remplir correctement les champs");
+//   }
+// });
